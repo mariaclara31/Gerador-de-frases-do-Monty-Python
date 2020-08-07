95649e73ef439f84c21fbef4f3e3b5afd4b91c7c
Maria Clara Monteiro <claramontier11@gmail.com>
2020-08-06 21:58:24 -0300

    Frases Monty Python

---
+++ src/main/java/challenge/QuoteRepository.java
@@ -0,0 +1,11 @@
+package challenge;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuoteRepository extends JpaRepository<Quote, Integer> {
+
+    List<Quote> findByActor(String actor);
+
+}
---
+++ src/main/java/challenge/RandomUtil.java
@@ -0,0 +1,9 @@
+package challenge;
+
+import java.util.Random;
+
+public class RandomUtil {
+    public static int generateRandomValue(int value) {
+        return new Random().nextInt(value);
+    }
+}
